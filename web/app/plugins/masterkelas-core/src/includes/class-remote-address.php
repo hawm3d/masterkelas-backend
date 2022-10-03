c465d104738ba41a2e4aee3544eda82c796ae4aa
Hamed Ataei <h4m3d99@gmail.com>
2022-10-03 19:19:10 +0330

    Forwarded ip fix

--- web/app/plugins/masterkelas-core/src/includes/class-remote-address.php
+++ web/app/plugins/masterkelas-core/src/includes/class-remote-address.php
@@ -13,7 +13,12 @@ namespace MasterKelas;
 class RemoteAddress {
 
   public static function Ip() {
-    $ip = (new self())->getIpAddress();
+    $ip_factory = new self();
+    $ip_factory->setUseProxy(true);
+    $ip_factory->setTrustedProxies([
+      "37.32.24.182", "37.32.25.95"
+    ]);
+    $ip = $ip_factory->getIpAddress();
     return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) ? $ip : null;
   }
 
